eaa9d725df9628e190ab083e93a0f8b63d90f233
lucassilva080397 <lucassilva080397@gmail.com>
2022-03-16 20:58:57 -0300

    refatorando tests

--- notificacoes_push_android/test/layers/data/repositories/get_all_notification_repository_test.dart
+++ notificacoes_push_android/test/layers/data/repositories/get_all_notification_repository_test.dart
@@ -2,13 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:notificacoes_push_android/layers/data/datasourcers/get_all_notification_datasource/get_all_notification_datasource.dart';
 import 'package:notificacoes_push_android/layers/data/datasourcers/get_all_notification_datasource/local/get_all_notification_local_datasource_imp.dart';
 import 'package:notificacoes_push_android/layers/data/repositories/get_all_notification_repository_imp.dart';
+import 'package:notificacoes_push_android/layers/domain/repositories/get_all_notification_repository%20copy.dart';
+import 'package:notificacoes_push_android/layers/services/local_storage/local_storage_service.dart';
+import 'package:notificacoes_push_android/layers/services/local_storage/sqflite/sqflite_local_storage_service_imp.dart';
 
 main() {
-  InitDatabaseUseCase database = InitDatabaseUseCaseImp(
-      InitDatabaseRepositoryImp(InitDatabaseDataSourceImp()));
+  LocalStorageService service = SqfliteLocalStorageServiceImp();
 
   GetAllNotificationDataSource dataSource =
-      GetAllNotificationLocalDataSourceImp(database);
+      GetAllNotificationLocalDataSourceImp(localStorageService: service);
   GetAllNotificationRepository repository =
       GetAllNotificationRepositoryImp(dataSource);
 
